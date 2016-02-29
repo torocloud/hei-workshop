376d1c2dba968f3428fbb1a858508130d5b03efb
jerrickpua <jerrickpua@gmail.com>
2016-02-29 16:08:32 +0800

    fix bug

--- s3-integrating-twitter-googlesheets/src/main/java/io/toro/workshop/blog/BlogApiController.java
+++ s3-integrating-twitter-googlesheets/src/main/java/io/toro/workshop/blog/BlogApiController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RestController
 @RequestMapping( value = "api/blogs" )
 public class BlogApiController {
